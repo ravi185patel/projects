0948527290ac5edf0087378fa224ced9b416e493
ravi patel <aryanrdp@gmail.com>
2025-12-26 10:35:03 +0530

    mybookingapp - mono structure ( booking only);

--- bookingapp/mybookingapp/src/main/java/com/ravidpatel/mybookingapp/controller/MovieController.java
+++ bookingapp/mybookingapp/src/main/java/com/ravidpatel/mybookingapp/controller/MovieController.java
@@ -17,7 +17,7 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-    @GetMapping("/")
+    @GetMapping("/{movie_name}")
     public List<MovieDto> getAllMovie(@PathVariable("movie_name") String movieName){
         return movieService.getAllMovie(new MovieDto(null,movieName));
     }
